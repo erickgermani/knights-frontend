799273684a54e30dfc57e14731d1c73e9e5ba670
Erick Germani <erickgermani13@gmail.com>
2024-07-29 10:42:32 -0300

    Removendo funções desnecessárias no KnightCardsComponent

--- src/components/knight-cards/KnightCardsComponent.vue
+++ src/components/knight-cards/KnightCardsComponent.vue
@@ -1,47 +1,10 @@
 <script setup lang="ts">
-import { ApiService, type SearchKnightsResponse } from '@/api/ApiService';
 import type { KnightEntity } from '@/entities/KnightEntity';
-import { KnightMapper } from '@/mappers/KnightMapper';
-import { onMounted, ref, type Ref } from 'vue';
 import KnightCardComponent from '../knight-card/KnightCardComponent.vue';
 
 defineProps<{
 	knights: KnightEntity[] | undefined;
 }>();
-
-const apiService = ApiService.getInstance();
-
-const loading = ref(false);
-
-let searchKnightsResult: SearchKnightsResponse;
-
-const searchedKnights: Ref<KnightEntity[]> = ref([]);
-
-const searchKnightsFailed = ref(false);
-
-async function searchKnights() {
-	searchKnightsFailed.value = false;
-	loading.value = true;
-
-	try {
-		searchKnightsResult =
-			(await apiService.searchKnights()) as SearchKnightsResponse;
-
-		if (searchKnightsResult.data === undefined) return [];
-
-		searchedKnights.value = searchKnightsResult.data.map((knightProps) =>
-			KnightMapper.toEntity(knightProps),
-		);
-	} catch (error) {
-		searchKnightsFailed.value = true;
-	} finally {
-		loading.value = false;
-	}
-}
-
-onMounted(async () => {
-	await searchKnights();
-});
 </script>
 
 <style lang="scss">
